521e8deec1611465a01b10740e4fd8d76760d1e4
royyhan <yantov3ah@gmail.com>
2024-12-24 00:15:38 +0700

    membuat web Nusantara Lobster

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// togel clas active
+const navbarNav = document.querySelector(".navbar-nav");
+//ketika menu di klik
+document.querySelector("#menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+//klik diluar saitbar untuk menghilangkan
+const menu = document.querySelector("#menu");
+
+document.addEventListener("click", function (e) {
+  if (!menu.contains(e.target) && !navbarNav.contains(e.target))
+    navbarNav.classList.remove("active");
+});
